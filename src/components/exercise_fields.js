b607f8419a96bd173b1eafc0626389f6e58b70b5
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-02-19 19:23:06 -0300

    add logic to save datas from a new training indexddb

--- src/components/exercise_fields.js
+++ src/components/exercise_fields.js
@@ -2,20 +2,50 @@ import React, { Component } from 'react';
 
 class ExerciseFields extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: null,
+      repetition: null,
+      weight: null
+    };
+  }
+
+  componentDidMount() {
+    this.nameInput.focus();
+  }
+
+  onInputChange(e) {
+    const splitted = e.target.id;
+    const key = splitted.split('_')[1];
+
+    this.setState({
+      [key]: e.target.value
+    });
+
+    const fieldId = splitted.split('_')[2];
+    this.props.onInputChange(fieldId, this.state);
+  }
+
   render() {
     return (
       <div className="row form-group">
         <div className="col">
           <label htmlFor={`exercise_name_${this.props.index}`}>Name</label>
-          <input type="text" className="form-control" id={`exercise_name_${this.props.index}`} />
+          <input type="text" className="form-control"
+           ref={ (input) => this.nameInput = input } id={`exercise_name_${this.props.index}`}
+           onChange={this.onInputChange.bind(this)} />
         </div>
         <div className="col">
           <label htmlFor={`exercise_repetition_${this.props.id}`}>Repetition</label>
-          <input type="text" className="form-control" id={`exercise_repetition_${this.props.index}`} />
+          <input type="text" className="form-control" id={`exercise_repetition_${this.props.index}`}
+          onChange={this.onInputChange.bind(this)} />
         </div>
         <div className="col">
           <label htmlFor={`exercise_weight_${this.props.id}`}>Weight</label>
-          <input type="text" className="form-control" id={`exercise_weight_${this.props.index}`} />
+          <input type="text" className="form-control" id={`exercise_weight_${this.props.index}`}
+          onChange={this.onInputChange.bind(this)} />
         </div>
         <div className="col">
           <button type="button" className="btn btn-danger"
--- src/components/register.js
+++ src/components/register.js
@@ -1,32 +1,79 @@
 import React, { Component } from 'react';
+import DB from '../libs/db';
 import ExerciseFields from './exercise_fields';
 
 class Register extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { exerciseComponents: [] };
+    this.state = {
+      titleField: null,
+      exerciseComponents: [],
+      exerciseFields: []
+    };
   }
 
-  onInputSubmit(e) {
-    console.log('submited');
+  onFormSubmit(e) {
     e.preventDefault();
+
+    const db = new DB();
+    db.createSchema();
+
+    db.insert({
+      title: this.state.titleField,
+      exercises: this.state.exerciseFields
+    });
+  }
+
+  onInputChange(e) {
+    this.setState({
+      titleField: e.target.value
+    });
   }
 
   removeExercise(i) {
     let state = this.state.exerciseComponents;
     state.splice(i, 1);
+
     this.setState({ exerciseComponents: state });
+  }
+
+  _updateExerciseFields(fieldId, params) {
+    let fields = this.state.exerciseFields;
+    const index = fields.map((x) => { return Object.keys(x)[0]; })
+                        .indexOf(fieldId);
+
+    if (index >= 0) {
+      fields[index][fieldId] = params;
+    } else {
+      fields.push({
+        [fieldId]: params
+      });
+    }
+
+    return fields;
+  }
+
+  onExerciseInputChange(fieldId, params) {
+    const fields = this._updateExerciseFields(fieldId, params);
+
+    this.setState({
+      exerciseFields: fields
+    });
   }
 
   onButtonAddExercise() {
     let exerciseComponents = this.state.exerciseComponents;
-    let id = exerciseComponents.length;
+    const id = exerciseComponents.length;
+    const props = {
+      index: id,
+      onButtonRemoveExercise: this.removeExercise.bind(this),
+      onInputChange: this.onExerciseInputChange.bind(this)
+    }
 
     this.setState({
       exerciseComponents: exerciseComponents.concat(
-        <ExerciseFields key={ id } index={ id }
-         onButtonRemoveExercise={ this.removeExercise.bind(this) } />)
+        <ExerciseFields key={ id } {...props} />)
     });
   }
 
@@ -35,10 +82,10 @@ class Register extends Component {
       <div>
         <h1>Register a new training</h1>
 
-          <form onSubmit={this.onInputSubmit}>
+          <form onSubmit={this.onFormSubmit.bind(this)}>
             <div className="form-group">
-              <label htmlFor="title">Title</label>
-              <input className="form-control" id="title" />
+              <label htmlFor="title" value={this.state.titleField}>Title</label>
+              <input className="form-control" id="title" onChange={this.onInputChange.bind(this)} />
             </div>
 
             {
