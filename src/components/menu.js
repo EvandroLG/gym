6200909ab091e35a39190551e8c3075f5640b2db
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-02-27 21:12:43 -0300

    update ux

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,8 +5,12 @@ class Menu extends Component {
   render() {
     return (
       <ul className="nav">
-        <li><NavLink to="/" className="nav-link">Current Training</NavLink></li>
-        <li><NavLink to="register" className="nav-link">New Training</NavLink></li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">Current Training</NavLink>
+        </li>
+        <li>
+          <NavLink to="register" className="nav-link">New Training</NavLink>
+        </li>
       </ul>
     )
   }
