9c09b4db0dc71ca556c23baf31d4941e9bc6ec22
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-03-15 22:32:05 -0300

    update webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,21 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
+
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015']
-      }
+      loader: 'babel-loader'
     }]
   },
+
   resolve: {
     extensions: ['.js', '.jsx']
   },
+
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
