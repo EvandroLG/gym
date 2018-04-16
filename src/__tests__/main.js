ee2dc81aeae703c01ccd6fddae7478d3edbbe147
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-04-15 21:24:19 -0300

    update tests - they are using snapshots to check their outputs

--- src/__tests__/main.js
+++ src/__tests__/main.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { HashRouter, Route } from 'react-router-dom';
-import Menu from '../components/menu';
-import TrainingList from '../components/training_list/index';
-import NewTraining from '../components/new_training/index';
+import toJson from 'enzyme-to-json';
 import Main from '../components/main';
 
 describe('main', () => {
-  const component = shallow(<Main />);
 
   it('should render as expected', () => {
-    expect(component.find(HashRouter)).toHaveLength(1);
-    expect(component.find(Route)).toHaveLength(2);
-    expect(component.find(Menu)).toHaveLength(1);
-    expect(component.find('.content')).toHaveLength(1);
-  });
+    const component = shallow(<Main />);
+    const tree = toJson(component);
 
-  it('should render route as expected', () => {
-    const first = component.find(Route).first().props();
-    expect(first.exact).toBeTruthy();
-    expect(first.path).toEqual('/');
-    expect(first.component).toEqual(TrainingList);
-
-    const last = component.find(Route).last().props();
-    expect(last.exact).toBeFalsy();
-    expect(last.path).toEqual('/new_training');
-    expect(last.component).toEqual(NewTraining);
+    expect(tree).toMatchSnapshot();
   });
 });
--- src/__tests__/menu.js
+++ src/__tests__/menu.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Menu from '../components/menu';
-import { NavLink } from 'react-router-dom';
 
 describe('menu', () => {
-  const component = shallow(<Menu />);
 
   it('component should render as expected', () => {
-    expect(component.find('.nav')).toHaveLength(1);
-    expect(component.find('.nav > li')).toHaveLength(2);
-    expect(component.find('.nav > li').first().find(NavLink)).toBeTruthy();
-    expect(component.find('.nav > li').last().find(NavLink)).toBeTruthy();
-  });
+    const component = shallow(<Menu />);
+    const tree = toJson(component);
 
-  it('component should render navLink component as expected', () => {
-    const first = component.find('.nav > li').first().find(NavLink).props();
-    expect(first.children).toEqual('Training');
-    expect(first.to).toEqual('/');
-    expect(first.exact).toBeTruthy();
-
-    const last = component.find('.nav > li').last().find(NavLink).props();
-    expect(last.children).toEqual('New Training');
-    expect(last.to).toEqual('/new_training');
-    expect(last.exact).toBeFalsy();
+    expect(tree).toMatchSnapshot();
   });
 });
