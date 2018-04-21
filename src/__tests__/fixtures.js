47a81a19fe925e7e40b6f6aac19155c63c96d0c1
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-04-20 21:53:09 -0300

    create fixtures file in tests directory

---
+++ src/__tests__/fixtures.js
@@ -0,0 +1,33 @@
+export default {
+  "legs": [
+    {
+      "name": "Barbell Deadlift",
+      "repetition": "8",
+      "set": "3",
+      "weight": "10kg"
+    },
+
+    {
+      "name": "Wide-Grip Pull-Up",
+      "repetition": "8",
+      "set": "3",
+      "weight": "35kg"
+    }
+  ],
+
+  "chest": [
+    {
+      "name": "Flat Bench Dumbbell Press",
+      "repetition": "8",
+      "set": "3",
+      "weight": "22kg"
+    },
+
+    {
+      "name": "Barbell Bench Press",
+      "repetition": "8",
+      "set": "3",
+      "weight": "25kg"
+    }
+  ]
+}
