1988d200e8faabca33816f97e5a952611788f8fc
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-05-04 10:34:49 -0300

    refactoring reducers using combineReducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,16 +1,14 @@
+import { combineReducers } from 'redux';
 import { SET_VIDEO_URL } from './actions';
 
-const DEFAULT_STATE = {
-  videoUrl: ''
-};
-
-const setVideoUrl = (state, action) => {
-  return Object.assign({}, state, { videoUrl: action.payload });
-};
-
-export default function rootReducer(state = DEFAULT_STATE, action) {
-  switch (action.type) {
-    case SET_VIDEO_URL: return setVideoUrl(state, action)
-    default: return state
+const videoUrl = (state='', action) => {
+  if (action.type === SET_VIDEO_URL) {
+    return action.payload;
   }
-}
+
+  return state;
+};
+
+export default combineReducers({
+  videoUrl
+});
