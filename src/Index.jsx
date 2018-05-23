a380baa7f7ddbca27364ebe6cd215c48a44a3a37
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-05-23 18:12:50 -0300

    add redux-thunk on the project

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './reducers';
+import store from './store';
 import Main from './components/Main';
 
 import './Index.css';
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 class App extends Component {
   render() {
