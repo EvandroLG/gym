2cf452a7d0c1d4f052e9f9afe69eea9652d29a4d
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-05-29 15:28:40 -0300

    update reducers tests

--- src/__tests__/fixtures.js
+++ src/__tests__/fixtures.js
@@ -4,6 +4,7 @@ export default [
     "title": "Leg",
     "exerciseList": [
       {
+        "id": 0,
         "name": "Barbell Deadlift",
         "repetition": "8",
         "set": "3",
@@ -11,6 +12,7 @@ export default [
       },
 
       {
+        "id": 1,
         "name": "Wide-Grip Pull-Up",
         "repetition": "8",
         "set": "3",
@@ -24,6 +26,7 @@ export default [
     "title": "Chest",
     "exerciseList": [
       {
+        "id": 0,
         "name": "Flat Bench Dumbbell Press",
         "repetition": "8",
         "set": "3",
@@ -31,6 +34,7 @@ export default [
       },
 
       {
+        "id": 1,
         "name": "Barbell Bench Press",
         "repetition": "8",
         "set": "3",
--- src/__tests__/reducers.js
+++ src/__tests__/reducers.js
@@ -2,22 +2,77 @@ import reducers from '../reducers';
 import fixtures from './fixtures';
 
 describe('reducers', () => {
-  it('should return an object with the same value passed as a parameter', () => {
-    const videoUrl = 'https://www.youtube.com/watch?v=04p4MWfhpAI';
-    const exerciseList = fixtures[0].exercises
-    const title = fixtures[0].title
 
-    const state = reducers({
-      videoUrl: ''
-    }, {
-      type: 'SET_VIDEO_URL',
-      url: videoUrl,
-      newTraining: {
-        exerciseList,
+  describe('video url', () => {
+    it('should handle SET_VIDEO_URL', () => {
+      const url = 'https://www.youtube.com/watch?v=04p4MWfhpAI';
+
+      const state = reducers(undefined, {
+        type: 'SET_VIDEO_URL',
+        url
+      });
+
+      expect(state).toMatchSnapshot();
+    });
+  });
+
+  describe('new training', () => {
+    it('should handle SET_TITLE_NEW_TRAINING', () => {
+      const { title } = fixtures[0];
+      const state = reducers(undefined, {
+        type: 'SET_TITLE_NEW_TRAINING',
         title
-      }
+      });
+
+      expect(state).toMatchSnapshot();
     });
 
-    expect(state).toMatchSnapshot();
+    it('should handle ADD_EXERCISE_NEW_TRAINING', () => {
+      const state = reducers(undefined, {
+        type: 'ADD_EXERCISE_NEW_TRAINING',
+      });
+
+      expect(state).toMatchSnapshot();
+    });
+
+    it('should handle REMOVE_EXERCISE_NEW_TRAINING', () => {
+      const state = reducers({
+        videoUrl: '',
+        trainingList: [],
+        newTraining: fixtures[0]
+      }, {
+        type: 'REMOVE_EXERCISE_NEW_TRAINING',
+        id: 0
+      });
+
+      expect(state).toMatchSnapshot();
+    });
+
+    it('should handle SET_FIELD_NEW_TRAINING', () => {
+      const state = reducers({
+        videoUrl: '',
+        trainingList: [],
+        newTraining: fixtures[0]
+      }, {
+        type: 'SET_FIELD_NEW_TRAINING',
+        id: 0,
+        fieldName: 'name',
+        value: 'Pull-Down'
+      });
+
+      expect(state).toMatchSnapshot();
+    });
+
+    it('should handle EMPTY_NEW_TRAINING', () => {
+      const state = reducers({
+        videoUrl: '',
+        trainingList: [],
+        newTraining: fixtures[0]
+      }, {
+        type: 'EMPTY_NEW_TRAINING',
+      });
+
+      expect(state).toMatchSnapshot();
+    });
   });
 });
