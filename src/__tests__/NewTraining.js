a969211d4b70d6d667c06f1f3fb3b8f687eea2dc
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-05-29 11:38:10 -0300

    improve tests in new training component

--- src/__tests__/NewTraining.js
+++ src/__tests__/NewTraining.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import DB from '../libs/db';
+import fixtures from './fixtures';
 import NewTraining from '../components/new_training/Index';
 import ExerciseFields from '../components/new_training/ExerciseFields';
 
@@ -8,22 +8,7 @@ import ExerciseFields from '../components/new_training/ExerciseFields';
 
 describe('new training', () => {
 
-  describe('instance', () => {
-    it('new training component should render as expected', () => {
-      const props = { title: '', exerciseList: [] };
-      const newTraining = mount(<NewTraining { ...props } />);
-
-      verifySnapshot(newTraining);
-    });
-
-    it('exercise fields component should render as expected', () => {
-      const exerciseFields = shallow(<ExerciseFields />);
-      verifySnapshot(exerciseFields);
-    });
-  });
-
-  describe('events', () => {
-
+  describe('new training component', () => {
     function mountNewTraining(keyProp, mockCallback) {
       const props = {
         title: '',
@@ -34,12 +19,17 @@ describe('new training', () => {
       return mount(<NewTraining { ...props } />);
     }
 
+    it('new training component should render as expected', () => {
+      const newTraining = mountNewTraining();
+      verifySnapshot(newTraining);
+    });
+
     it('should call correct callback after click on add button', () => {
       const mockCallback = jest.fn();
       const newTraining = mountNewTraining('onAddExercise', mockCallback) 
 
       newTraining.find('#add').simulate('click');
-      expect(mockCallback.mock.calls.length).toBe(1);
+      expect(mockCallback).toBeCalled();
     });
 
     it('should call correct callback after change title', () => {
@@ -55,35 +45,62 @@ describe('new training', () => {
       expect(mockCallback).toMatchSnapshot();
     });
 
-    function mountExercise(keyProp, mockCallback) {
+    it('should call correct method after submit form', () => {
+      const { title, exerciseList } = fixtures[0];
+      const mockCallback = jest.fn();
+
+      const props = {
+        title,
+        exerciseList,
+        onSubmit: mockCallback
+      };
+
+      const newTraining = mount(<NewTraining { ...props } />);
+
+      newTraining.find('form').simulate('submit', {
+        preventDefault: () => {}
+      });
+
+      expect(mockCallback).toBeCalled();
+      expect(mockCallback).toBeCalledWith(title, exerciseList);
+    });
+  });
+
+  describe('exercise fields component', () => {
+    function mountExerciseFields(keyProperty, mockCallback) {
       const props = {
         id: 1,
-        title: '',
-        exerciseList: [],
-        [keyProp]: mockCallback
+        [keyProperty]: mockCallback
       };
 
       return shallow(<ExerciseFields { ...props } />);
     }
 
-    it('should call correct callback after change input', () => {
-      const mockCallback = jest.fn();
-      const exercise = mountExercise('onInputChange', mockCallback);
-      const id = 'exercise_name_1';
+    it('exercise fields component should render as expected', () => {
+      const exerciseFields = mountExerciseFields();
+      verifySnapshot(exerciseFields);
+    });
 
-      exercise.find(`#${id}`).simulate('change', {
+    it('should call correct method after change input', () => {
+      const mockCallback = jest.fn();
+      const exerciseFields = mountExerciseFields('onInputChange', mockCallback);
+      const id = 'exercise_name_1';
+      const value = 'Pull-Down';
+
+      exerciseFields.find(`#${id}`).simulate('change', {
         target: {
           id,
-          value: 'Dumbbel Press'
+          value
         }
       });
 
-      expect(mockCallback).toMatchSnapshot();
+      expect(mockCallback).toBeCalled();
+      expect(mockCallback).toBeCalledWith(1, 'name', value);
     });
 
     it('should call correct callback after click on remove button', () => {
       const mockCallback = jest.fn();
-      const exercise = mountExercise('onButtonRemoveExercise', mockCallback);
+      const exercise = mountExerciseFields('onButtonRemoveExercise', mockCallback);
 
       exercise.find('.remove').simulate('click');
 
