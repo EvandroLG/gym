15a93fcbe85c7f3085259be129b6397329e449a7
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-05-31 13:55:02 -0300

    remove action tests

--- src/__tests__/action.js
+++
@@ -1,9 +0,0 @@
-import { setVideoUrl } from '../action';
-
-describe('action', () => {
-
-  it('should return an expected object', () => {
-    const output = setVideoUrl('https://www.youtube.com/watch?v=04p4MWfhpAI');
-    expect(output).toMatchSnapshot();
-  });
-});
